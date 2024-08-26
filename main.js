99d3be9853f2caaf9ce013fae40342c74bdbb83f
Eagle Raven Global <grant@glendevonmarine.com>
2024-08-27 08:33:16 +1200

    Update main.js
    Bug fix for hourAmended. No longer showing negative number. Cleaned up code. Added more comments.

--- main.js
+++ main.js
@@ -6,40 +6,16 @@ const spaceDiv = document.getElementById('space');
 const minuteDiv = document.getElementById('minute');
 const dayDiv = document.getElementById("day");
 const dateDiv = document.getElementById("date");
-const ampmDiv = document.getElementById("ampm")
+const ampmDiv = document.getElementById("ampm");
 
 function getTime(){
     const now = new Date();
 
+    // TIME
     let hour = now.getHours();
     let minute = now.getMinutes();
     let second = now.getSeconds();
     
-    let hourAmended = hour - 12;
-    // TIME
-    // Adding 0 to hour and minute to look better
-
-
-    if(hourAmended < 10){
-        hourAmended = '0' + hourAmended;
-    }
-
-    if(minute < 10){
-        minute = '0' + minute;
-    }
-
-    hourDiv.textContent = hourAmended;
-    spaceDiv.textContent = ':';
-
-    if(second % 2 === 1){
-        spaceDiv.style.opacity = 1
-    }
-    else{
-        spaceDiv.style.opacity = 0;
-    }
-    
-    minuteDiv.textContent = minute;
-
     // DAY
     let day = now.getDay();
     const days_of_week = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
@@ -47,21 +23,49 @@ function getTime(){
 
     let date = now.getDate();
 
-    dayDiv.textContent = today
+    dayDiv.textContent = today;
     dateDiv.textContent = date;
 
+    let hourAmended = hour;
 
-    // AM/PM
-    if(hour < 12){
+    // TIME
+    // Adding 0 if below 10
+    if(hourAmended < 10){
+        hourAmended = '0' + hourAmended;
+    }
+
+    // Same as above for minute
+    if(minute < 10){
+        minute = '0' + minute;
+    }
+
+    // Making the hour 12 hour time
+    if(hour > 12 ){
+        hourAmended = hour - 12;
         ampmDiv.textContent = 'am';
     }
     else{
         ampmDiv.textContent = 'pm';
     }
 
-    console.log(second)
+
+    // Render to page
+    hourDiv.textContent = hourAmended;
+    spaceDiv.textContent = ':';
+
+    // Blinking effect for :
+    if(second % 2 === 1){
+        spaceDiv.style.opacity = 1;
+    }
+    else{
+        spaceDiv.style.opacity = 0;
+    }
+    
+    minuteDiv.textContent = minute;
+
+    console.log(second);
 }
 
-setInterval(getTime, 1000)
+setInterval(getTime, 1000);
 
 
